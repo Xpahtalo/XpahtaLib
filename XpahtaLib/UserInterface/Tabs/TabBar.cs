2aeba882ba9dd2b9d7828ca835dfd6ba74d882df
Xpahtalo <106037495+Xpahtalo@users.noreply.github.com>
2023-10-06 11:15:46 -0700

    add tabs

---
+++ XpahtaLib/UserInterface/Tabs/TabBar.cs
@@ -0,0 +1,34 @@
+﻿using Dalamud.Interface.Utility.Raii;
+using FFXIVClientStructs.FFXIV.Client.Graphics.Scene;
+
+namespace XpahtaLib.UserInterface.Tabs;
+
+public class TabBar : IDisposable
+{
+    // ReSharper disable once CollectionNeverUpdated.Global
+    public required List<TabBase> Tabs { get; set; }
+
+    public void Draw()
+    {
+        using var tabBar = ImRaii.TabBar("Main Window Tab Bar");
+        if (!tabBar)
+            return;
+        for (var i = 0; i < Tabs.Count; i++) {
+            using var id = ImRaii.PushId(i);
+            Tabs[i].Draw();
+        }
+    }
+    
+    
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing) { }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ XpahtaLib/UserInterface/Tabs/TabBase.cs
@@ -0,0 +1,30 @@
+﻿using Dalamud.Interface.Utility.Raii;
+using ImGuiNET;
+
+namespace XpahtaLib.UserInterface.Tabs;
+
+public abstract class TabBase : IDisposable
+{
+    public abstract string Name { get; }
+    
+    public virtual void Draw()
+    {
+        using var tab = ImRaii.TabItem(Name);
+        if (!tab)
+            return;
+        DrawTab();
+    }
+
+    protected virtual void DrawTab() { ImGui.Text("Not implemented yet."); }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing) { }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
