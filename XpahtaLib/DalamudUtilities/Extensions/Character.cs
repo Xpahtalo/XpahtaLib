394f66aebbc42a3e3106ea6f209a72af42c899da
Xpahtalo <106037495+Xpahtalo@users.noreply.github.com>
2023-11-10 09:15:37 -0800

    add Character.cs and Job.cs to make it easier to work with job info

---
+++ XpahtaLib/DalamudUtilities/Extensions/Character.cs
@@ -0,0 +1,18 @@
+﻿using Dalamud.Game.ClientState.Objects.Types;
+
+namespace XpahtaLib.DalamudUtilities.Extensions;
+
+public static class CharacterExtensions
+{
+    public static Job Job(this Character character)
+    {
+        var jobId = character.ClassJob.Id;
+        if (jobId < JobExtensions.DefinedJobCount) {
+            return (Job)jobId;
+        }
+
+        return DalamudUtilities.Job.Unknown;
+    }
+
+    public static bool IsJob(this Character character, Job job) => character.Job() == job;
+}
